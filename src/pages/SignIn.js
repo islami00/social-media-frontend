2c33be6b507fc08771d04615c780ad6f574a03bb
Islam <islamkwnd@gmail.com>
2022-06-04 21:29:56 +0000

    feat(signin): Added blob, fixed form submit
    Cropped green blob for placement, nested everything in form el for submit to work

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -41,7 +41,7 @@ export const SignInPage = () => {
       .then(async (result) => {
         if (result[1]) throw result[1];
         const data = await result[0].json();
-        if (!data[0]) throw new Error("Cannot fetch");
+        if (!data[0]) throw new Error("No data on json server");
         localStorage.setItem("user", JSON.stringify(data[0]));
         navigate("/main/home");
       })
@@ -79,69 +79,73 @@ export const SignInPage = () => {
     else setPwdBox({ type: "password", "data-state": "invisible" });
   }
   return (
-    <div className="h-[896px] w-[414px] p-4">
-      <nav className="w-full">
-        <button aria-label="Back">
+    <div className="h-screen w-screen lg:w-[414px] p-4 flex flex-col justify-between sign-in">
+      <nav className="w-full flex">
+        <button
+          aria-label="Back"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
           <MdOutlineArrowBack size={24} />
         </button>
       </nav>
-      <section className="w-full">
-        <div className="text-center">
-          <h1 className="text-6xl font-bold">Hello</h1>
-          <p>Sign in to your account</p>
-        </div>
-        <form
-          onSubmit={handleSubmit(submitData)}
-          className="flex flex-col gap-6 items-center pt-12 pb-6  w-full px-2 "
-        >
-          <Input
-            leftIcon={<MdAccountCircle size={24} />}
-            className="form-control"
-            {...register("username", { required: true })}
-            placeholder="Enter your username"
-          />
-          <Input
-            leftIcon={<MdLock size={24} />}
-            className="form-control"
-            {...pwdBox}
-            {...register("password", { required: true })}
-            placeholder="Your password goes here"
-            rightIcon={[
-              <MdOutlineVisibility
-                key={crypto?.randomUUID()}
-                size={24}
-                className="password-visible"
-              />,
-              <MdOutlineVisibilityOff
-                key={crypto?.randomUUID()}
-                size={24}
-                className="password-invisible"
-              />,
-            ]}
-            onRightIconClick={handleIcon}
-          />
-          <Link className="text-violet-400 self-end" to="/signup">
-            Forgot password?
-          </Link>
-        </form>
-      </section>
-      <section className="flex flex-col gap-6 w-full pt-12 items-center">
-        <label className="self-end font-bold inline-flex items-center gap-2">
-          Log in
-          <button
-            type="submit"
-            className="rounded-full px-3 py-1 self-center btn-primary"
-          >
-            <MdOutlineArrowForward color="white" size={24} />
-          </button>
-        </label>
-        <p>
-          Don't have an account?{" "}
-          <Link to="/signup" className="font-bold underline">
-            Create
-          </Link>
-        </p>
-      </section>
+      <form onSubmit={handleSubmit(submitData)}>
+        <section className="w-full">
+          <div className="text-center">
+            <h1 className="text-6xl font-bold">Hello</h1>
+            <p>Sign in to your account</p>
+          </div>
+          <div className="flex flex-col gap-6 items-center pt-12 pb-6  w-full px-2 ">
+            <Input
+              leftIcon={<MdAccountCircle size={24} />}
+              className="form-control"
+              {...register("username", { required: true })}
+              placeholder="Username"
+            />
+            <Input
+              leftIcon={<MdLock size={24} />}
+              className="form-control"
+              {...pwdBox}
+              {...register("password", { required: true })}
+              placeholder="Password"
+              rightIcon={[
+                <MdOutlineVisibility
+                  key={crypto?.randomUUID()}
+                  size={24}
+                  className="password-visible"
+                />,
+                <MdOutlineVisibilityOff
+                  key={crypto?.randomUUID()}
+                  size={24}
+                  className="password-invisible"
+                />,
+              ]}
+              onRightIconClick={handleIcon}
+            />
+            <Link className="text-violet-400 self-end" to="/signup">
+              Forgot password?
+            </Link>
+          </div>
+        </section>
+        <section className="flex flex-col gap-6 w-full pt-12 items-center px-2">
+          <label className="self-end font-bold inline-flex items-center gap-2">
+            Sign in
+            <button
+              type="submit"
+              className="rounded-full px-3 py-1 self-center btn-primary"
+            >
+              <MdOutlineArrowForward color="white" size={24} />
+            </button>
+          </label>
+          <p>
+            Don't have an account?{" "}
+            <Link to="/signup" className="font-bold underline">
+              Create
+            </Link>
+          </p>
+        </section>
+      </form>
       <IonToast
         duration={2000}
         isOpen={error}
